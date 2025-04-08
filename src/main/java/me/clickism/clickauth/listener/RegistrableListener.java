323704a4576388794b99cfba215e3ffa0a90b91d
Clickism <yagizaktasx@gmail.com>
2025-04-08 21:52:38 +0200

    add RegistrableListener to easily register listeners

---
+++ src/main/java/me/clickism/clickauth/listener/RegistrableListener.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2020-2025 Clickism
+ * Released under the GNU General Public License 3.0.
+ * See LICENSE.md for details.
+ */
+
+package me.clickism.clickauth.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public interface RegistrableListener extends Listener {
+    @SuppressWarnings("unchecked")
+    default <T extends RegistrableListener> T registerListener(JavaPlugin plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+        return (T) this;
+    }
+}
