853261e11c443203926826ea8dc02d28e973264d
Clickism <yagizaktasx@gmail.com>
2025-05-12 23:47:57 +0200

    use sha-256 for ip hashing

--- src/main/java/me/clickism/clickauth/authentication/IpHasher.java
+++ src/main/java/me/clickism/clickauth/authentication/IpHasher.java
@@ -6,14 +6,30 @@
 
 package me.clickism.clickauth.authentication;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class IpHasher implements Hasher {
+    private final MessageDigest digest;
+
+    public IpHasher() {
+        try {
+            this.digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 not supported", e);
+        }
+    }
+
     @Override
     public String hash(String string) {
-        return String.valueOf(string.hashCode());
+        byte[] hashBytes = digest.digest(string.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(hashBytes);
     }
 
     @Override
     public boolean check(String string, String hash) {
-        return String.valueOf(string.hashCode()).equals(hash);
+        return hash(string).equals(hash);
     }
 }
